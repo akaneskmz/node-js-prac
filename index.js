7d1712ae86b87acf7190db5396dee0fa99ad368e
akaneskmz <vermilion_sounds@yahoo.co.jp>
2020-12-03 23:48:18 +0900

    get_steam_screenshots作成

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+"use strict";
+
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+const get_steam_screenshots = require('./routes/get-steam-screenshots');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .get('/get-steam-screenshots', get_steam_screenshots)
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
