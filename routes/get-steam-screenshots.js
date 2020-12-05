e95dc240b829cec965101d0939e7ca37accbcec1
akaneskmz <vermilion_sounds@yahoo.co.jp>
2020-12-05 18:43:40 +0900

    デバッグログ追加

--- routes/get-steam-screenshots.js
+++ routes/get-steam-screenshots.js
@@ -17,6 +17,7 @@ const get_steam_screenshots = (req, res) => {
   request({ url: LIST_URL, encoding: null },
         async (error, response, body) => {
             if (!error && response.statusCode == 200) {
+                console.logs(body);
                 const client = await pool.connect();
                 const $ = cheerio.load(body);
                 let json = {};
