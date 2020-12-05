8af504a3045539a13da7030545dc00d1d2c35ca4
akaneskmz <vermilion_sounds@yahoo.co.jp>
2020-12-05 19:12:17 +0900

    フォーマット

--- routes/get-steam-screenshots.js
+++ routes/get-steam-screenshots.js
@@ -15,80 +15,80 @@ const pool = new Pool({
 const get_steam_screenshots = (req, res) => {
   console.log(LIST_URL);
   request({ url: LIST_URL, encoding: null },
-        async (error, response, body) => {
-            if (!error && response.statusCode == 200) {
-                console.log(body);
-                const client = await pool.connect();
-                const $ = cheerio.load(body);
-                let json = {};
-                // DBからタイトルJSON取得
-                try {
-                  const result = await client.query('SELECT json FROM steam_screenshots_title where id = 0');
-                  json = result["rows"][0]["json"];
-                  console.log(json);
-                } catch (e) {
-                  res.send(e);
-                  return;
-                }
+    async (error, response, body) => {
+      if (!error && response.statusCode == 200) {
+        console.log(body);
+        const client = await pool.connect();
+        const $ = cheerio.load(body);
+        let json = {};
+        // DBからタイトルJSON取得
+        try {
+          const result = await client.query('SELECT json FROM steam_screenshots_title where id = 0');
+          json = result["rows"][0]["json"];
+          console.log(json);
+        } catch (e) {
+          res.send(e);
+          return;
+        }
 
-                let p = [];
-                let screenshots = []
-                $(".profile_media_item").each((i, elem) => {
-                  console.logs('each profile_media_item');
-                  let ss = {}
-                  screenshots.push(ss);
-                  p.push(new Promise((resolve, reject) => {
-                    const q = $('.imgWallHoverDescription q', elem);
-                    const title = q.length ? q.text() : "(untitled)";
-                    ss['title'] = title
-                    console.logs("title: " + title);
-                    const node = $('div.imgWallItem', elem);
-                    const node_id = node['0']['attribs']['id'];
-                    const item_id = node_id.replace('imgWallItem_', '');
-                    const link = "http://steamcommunity.com/sharedfiles/filedetails/?id=" + item_id;
-                    ss['link'] = link
-                    const thumb = node['0']['attribs']['style'].split("'")[1]
-                    ss['thumb'] = thumb
-  
-                    // ゲーム名取得
-                    let game_title = "";
-                    if (item_id in json) {
-                      game_title = json[item_id];
-                      ss['game_title'] = game_title
-                      resolve(game_title);
-                    } else {
-  
-                      request({url: link, headers: {'Accept-Language': 'ja,en-US'}}).then((html) => {
-                        const $$ = cheerio.load(html);
-                        const h3 = $$('h3.apphub_responsive_menu_title');
-                        const a = $$('div.screenshotAppName a');
-                        game_title = h3.text() != ''? h3.text(): a.text();
-                        json[item_id] = game_title;
-                      }).then(() => {
-                        ss['game_title'] = game_title
-                        resolve(game_title);
-                      });
-                    }
-                  }));
-                });
-                let pall = Promise.all(p).then((t) => {
-                  
-                  // DBからタイトルJSON取得
-                  client.query('update steam_screenshots_title set json = $1 where id = 0', [json])
-                  .then((result) => {
-                    console.log("update " + result.rowCount);
-                  })
-                  .finally(() => {
-                    console.log("p.then: ");
-                    console.log(p);
-                    client.release();
-                  });
-                  
-                  let data = {screenshots: screenshots};
-                  res.render('pages/get_steam_screenshots', data);
-                });
+        let p = [];
+        let screenshots = []
+        $(".profile_media_item").each((i, elem) => {
+          console.log('each profile_media_item');
+          let ss = {}
+          screenshots.push(ss);
+          p.push(new Promise((resolve, reject) => {
+            const q = $('.imgWallHoverDescription q', elem);
+            const title = q.length ? q.text() : "(untitled)";
+            ss['title'] = title
+            console.logs("title: " + title);
+            const node = $('div.imgWallItem', elem);
+            const node_id = node['0']['attribs']['id'];
+            const item_id = node_id.replace('imgWallItem_', '');
+            const link = "http://steamcommunity.com/sharedfiles/filedetails/?id=" + item_id;
+            ss['link'] = link
+            const thumb = node['0']['attribs']['style'].split("'")[1]
+            ss['thumb'] = thumb
+
+            // ゲーム名取得
+            let game_title = "";
+            if (item_id in json) {
+              game_title = json[item_id];
+              ss['game_title'] = game_title
+              resolve(game_title);
+            } else {
+
+              request({ url: link, headers: { 'Accept-Language': 'ja,en-US' } }).then((html) => {
+                const $$ = cheerio.load(html);
+                const h3 = $$('h3.apphub_responsive_menu_title');
+                const a = $$('div.screenshotAppName a');
+                game_title = h3.text() != '' ? h3.text() : a.text();
+                json[item_id] = game_title;
+              }).then(() => {
+                ss['game_title'] = game_title
+                resolve(game_title);
+              });
             }
+          }));
         });
+        let pall = Promise.all(p).then((t) => {
+
+          // DBからタイトルJSON取得
+          client.query('update steam_screenshots_title set json = $1 where id = 0', [json])
+            .then((result) => {
+              console.log("update " + result.rowCount);
+            })
+            .finally(() => {
+              console.log("p.then: ");
+              console.log(p);
+              client.release();
+            });
+
+          let data = { screenshots: screenshots };
+          res.render('pages/get_steam_screenshots', data);
+        });
+      }
+    });
 };
 
 module.exports = get_steam_screenshots;
